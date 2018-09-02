67a96146ee12668b4a340aeae0551fa07b56ef45
Ivo Batistela <irbatistela@gmail.com>
2018-09-02 16:46:33 -0300

    commit the last deduction. All requirements, deductions and addition are working

---
+++ src/main/java/rocks/byivo/passwordmeter/measure/score/deductions/RepeatedCharactersDeduction.java
@@ -0,0 +1,99 @@
+package rocks.byivo.passwordmeter.measure.score.deductions;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RepeatedCharactersDeduction implements PasswordScoreDeduction {
+
+    @Override
+    public long getTotalBonusFrom(String rawPassword) {
+	int passwordLength = rawPassword.length();
+	RepetetitionFinalScore finalScore = new RepetetitionFinalScore(passwordLength);
+	
+	for (int i = 0; i < passwordLength; i++) {
+
+	    InnerSequenceScore resultOfSumByRepeatedDistance = findAllRepetitionsOfActualIndexCharacter(rawPassword, i);
+	    if (resultOfSumByRepeatedDistance.foundRepeated()) {
+		finalScore = finalScore.sumRepeatedScore(resultOfSumByRepeatedDistance.totalInnerScore);
+	    }
+	}
+
+	return (int) finalScore.finalScore;
+    }
+
+    private InnerSequenceScore findAllRepetitionsOfActualIndexCharacter(String rawPassword, int indexOnMainLoop) {
+	InnerSequenceScore result = new InnerSequenceScore();
+	int passwordLength = rawPassword.length();
+	char actualOnMainLoop = rawPassword.charAt(indexOnMainLoop);
+	
+	for (int indexOnInnerLoop = 0; indexOnInnerLoop < passwordLength; indexOnInnerLoop++) {
+	    char actualOnInnerLoop = rawPassword.charAt(indexOnInnerLoop);
+	    
+	    boolean isRepetitionsInDiferentIndex = actualOnMainLoop == actualOnInnerLoop && indexOnMainLoop != indexOnInnerLoop;
+	    if (isRepetitionsInDiferentIndex) {
+		double absoluteValueBasedOnDistance = Math.abs(passwordLength * 1.0 / (indexOnInnerLoop - indexOnMainLoop));
+		result = result.addAbsoluteNumberOfRepetitionDistance(absoluteValueBasedOnDistance);
+	    }
+	}
+	
+	return result;
+    }
+
+    private final class InnerSequenceScore {
+
+	final boolean foundRepeated;
+	final double totalInnerScore;
+
+	private InnerSequenceScore() {
+	    foundRepeated = false;
+	    totalInnerScore = .0;
+	}
+
+	public InnerSequenceScore(boolean foundRepeated, double totalInnerScore) {
+	    super();
+	    this.foundRepeated = foundRepeated;
+	    this.totalInnerScore = totalInnerScore;
+	}
+
+	private final InnerSequenceScore addAbsoluteNumberOfRepetitionDistance(double newScore) {
+	    double summedScore = totalInnerScore + newScore;
+	    return new InnerSequenceScore(true, summedScore);
+	}
+	
+	private final boolean foundRepeated() {
+	    return this.foundRepeated;
+	}
+    }
+    
+    private final class RepetetitionFinalScore {
+
+	final int countOfRepetitions;
+	final double finalScore;
+	final int passwordLength;
+
+	private RepetetitionFinalScore(int passwordLength) {
+	    this.passwordLength = passwordLength;
+	    countOfRepetitions = 0;
+	    finalScore = .0;
+	}
+
+	public RepetetitionFinalScore(int countOfRepetitions, double finalScore, int passwordLength) {
+	    this.countOfRepetitions = countOfRepetitions;
+	    this.finalScore = finalScore;
+	    this.passwordLength = passwordLength;
+	}
+
+	private final RepetetitionFinalScore sumRepeatedScore(double newScore) {
+	    double summedScore = finalScore + newScore;
+	    int newNumberOfRepetitions = countOfRepetitions + 1;
+
+	    int countOfUniqueChars = passwordLength - newNumberOfRepetitions;
+	    if (countOfUniqueChars > 0) {
+		summedScore = summedScore / countOfUniqueChars;
+	    }
+
+	    return new RepetetitionFinalScore(newNumberOfRepetitions, Math.ceil(summedScore), passwordLength);
+	}
+    }
+
+}
---
+++ src/test/java/rocks/byivo/passwordmeter/measure/score/deductions/RepeatedCharactersDeductionTest.java
@@ -0,0 +1,60 @@
+package rocks.byivo.passwordmeter.measure.score.deductions;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RepeatedCharactersDeductionTest {
+    
+    private RepeatedCharactersDeduction repeatedCharactersDeduction;
+    
+    @Before
+    public void setUp() throws Exception {
+	repeatedCharactersDeduction = new RepeatedCharactersDeduction();
+    }
+
+    @Test
+    public void scoreAmountIfBasedOnPasswordLengthDividedByDifferenceOfDistanceBetweenIdenticalCharacters() {
+	long totalDeductionFromPassword = repeatedCharactersDeduction.getTotalBonusFrom("aa");
+	assertThat(totalDeductionFromPassword, is(4L));
+    }
+    
+    @Test
+    public void theScorShouldBeSmallerIfTheDistanceBetweenIdenticalCharactersAreBig() {
+	long totalDeductionFromPassword = repeatedCharactersDeduction.getTotalBonusFrom("a1234a");
+	assertThat(totalDeductionFromPassword, is(1l));
+    }
+    
+    @Test
+    public void theScoreGrowsWayBiggerIfThereIsManyOfOneCharacter() {
+	long totalDeductionFromPassword = repeatedCharactersDeduction.getTotalBonusFrom("aaaaaaaaaa");
+	assertThat(totalDeductionFromPassword, is(97l));
+    }
+    
+    @Test
+    public void theScoreDoesNotGrowTooMuchIfThereIsManySmallRepetitions() {
+	long totalDeductionFromPassword = repeatedCharactersDeduction.getTotalBonusFrom("abcdeabcde");
+	assertThat(totalDeductionFromPassword, is(6l));
+    }
+    
+    @Test
+    public void theRepetitionsCheckerProcessShouldBeCaseSensitive() {
+	long totalDeductionFromPassword = repeatedCharactersDeduction.getTotalBonusFrom("AaAaA");
+	assertThat(totalDeductionFromPassword, is(11l));
+    }
+    
+    @Test
+    public void theRepetitionsShouldAlsoScoreSymbols() {
+	long totalDeductionFromPassword = repeatedCharactersDeduction.getTotalBonusFrom("####");
+	assertThat(totalDeductionFromPassword, is(25l));
+    }
+    
+    @Test
+    public void theRepetitionsShouldAlsoScoreNumbers() {
+	long totalDeductionFromPassword = repeatedCharactersDeduction.getTotalBonusFrom("1111");
+	assertThat(totalDeductionFromPassword, is(25l));
+    }
+    
+}
