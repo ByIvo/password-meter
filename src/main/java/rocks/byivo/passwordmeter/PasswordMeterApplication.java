3c4c80c70bd777d762b5e46f37028557e924d1b1
Ivo Batistela <irbatistela@gmail.com>
2018-09-03 00:35:16 -0300

    changes some info from startup

---
+++ src/main/java/rocks/byivo/passwordmeter/PasswordMeterApplication.java
@@ -0,0 +1,20 @@
+package rocks.byivo.passwordmeter;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class PasswordMeterApplication extends SpringBootServletInitializer{
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PasswordMeterApplication.class);
+    }
+    
+    public static void main(String[] args) {
+	SpringApplication.run(PasswordMeterApplication.class, args);
+    }
+}
